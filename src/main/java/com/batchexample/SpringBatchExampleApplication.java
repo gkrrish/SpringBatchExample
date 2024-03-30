972acc99d144cc5b1466a2b0c66e11f7080ef620
Krishna <gkrrish.11@gmail.com>
2024-03-30 10:47:16 +0530

    Closing the project, almost all poc are done using the batch

--- src/main/java/com/batchexample/SpringBatchExampleApplication.java
+++ src/main/java/com/batchexample/SpringBatchExampleApplication.java
@@ -13,6 +13,7 @@ public class SpringBatchExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchExampleApplication.class, args);
 		System.out.println("H2 DATABSE LOCAL : http://localhost:8284/h2-console");
+		System.out.println("http://localhost:8284/actuator/health");
 	}
 
 }
